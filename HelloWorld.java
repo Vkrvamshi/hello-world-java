7ea6c218541d3ead1843e263c39141fcfc9a6931
Vkrvamshi <vamshivkreddy9@gmail.com>
2025-12-09 22:10:45 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -35,7 +35,7 @@
 public class HelloWorld {
   public static void main(String[] args) {
      System.out.println("Hello, KrishnaDevOpsss");
-     System.out.println("Hello, devopsbyvamshi1");
+     System.out.println("Hello, devopsbyvamshi");
      System.out.println("Hello, devopsbyvamshi123"); 
 
   }
